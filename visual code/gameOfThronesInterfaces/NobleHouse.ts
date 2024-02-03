bdd3d7d513895a4fb2fd622b730eca6608c28197
Marcelo <marcelodsr1991@icloud.com>
2024-02-03 17:51:00 -0300

    atualizacao

--- visual code/gameOfThronesInterfaces/NobleHouse.ts
+++ visual code/gameOfThronesInterfaces/NobleHouse.ts
@@ -1,24 +1,29 @@
+import { Heir } from "./Heir"
 import { HouseHeir } from "./HouseHeir";
+
 export class NobleHouse {
     nameHouse: string; 
     motto: string; 
+    heirs: HouseHeir[] = [];
    
 
-    constructor(nameHouse: string, motto: string) {
-        this.nameHouse = nameHouse;
-        this.motto = motto;
-    }
-    public getNameHouse(): string {
-        return this.nameHouse;
-    }
-    public setNameHouse(value: string) {
-        this.nameHouse = value;
-    }
+constructor(nameHouse: string, motto: string) {
+    this.nameHouse = nameHouse;
+    this.motto = motto;
+}
 
-    public getMotto(): string {
-        return this.motto;
-    }
-    public setMotto(value: string) {
-        this.motto = value;
+addHeir(heir: HouseHeir): void {
+    this.heirs.push(heir);
+  }
+
+  printHouseInfo(): void {
+    console.log(`Casa Nobre: ${this.nameHouse}`);
+    console.log(`Lema: ${this.motto}`);
+    console.log("Herdeiros:");
+
+    for (let i = 0; i < this.heirs.length; i++) {
+        const heir = this.heirs[i];
+        console.log(`${i + 1}. ${heir.heirName}, Age: ${heir.heirAge}, position in succession: ${heir.successionNumber}`);
     }
 }
+}  
