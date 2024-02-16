baed8d1395cf6048194360b045cf5584c0ee435f
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-02-16 11:45:01 -0300

    Delete visual code/gameOfThronesInterfaces/HouseStark.ts

--- visual code/gameOfThronesInterfaces/HouseStark.ts
+++
@@ -1,7 +0,0 @@
-import { NobleHouse } from "./NobleHouse";
-import { HouseRules } from "./HouseRules";
-
-export class HouseStark extends NobleHouse implements HouseRules  {
-    nameHouse: string;
-    motto: string;
-}
