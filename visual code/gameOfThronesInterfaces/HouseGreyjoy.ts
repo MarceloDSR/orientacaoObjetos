bd3334e461c938699dbde5f3bfcf65c9905b75e6
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-02-16 11:44:44 -0300

    Delete visual code/gameOfThronesInterfaces/HouseGreyjoy.ts

--- visual code/gameOfThronesInterfaces/HouseGreyjoy.ts
+++
@@ -1,7 +0,0 @@
-import { HouseRules } from "./HouseRules";
-import { NobleHouse } from "./NobleHouse";
-
-export class HouseGreyjoy extends NobleHouse implements HouseRules {
-    nameHouse: string;
-    motto: string;
-}
