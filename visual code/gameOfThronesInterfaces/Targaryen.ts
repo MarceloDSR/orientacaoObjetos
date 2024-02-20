aa469dcc6e51027e5aac1e02e128041c21c84466
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-02-19 21:27:32 -0300

    Update Targaryen.ts

--- visual code/gameOfThronesInterfaces/Targaryen.ts
+++ visual code/gameOfThronesInterfaces/Targaryen.ts
@@ -1,9 +1,12 @@
 import { NobleHouse } from "./NobleHouse";
+import { Heir } from "./Heir";
+import { HouseRules } from "./HouseRules";
 
-export class Targaryen extends NobleHouse {
-    nameHouse: string; 
-    motto: string; 
-    numSoldiers: number;
+export class Targaryen extends NobleHouse implements HouseRules {
+  nameHouse: string; 
+  motto: string; 
+  numSoldiers: number;
+  heirs: Heir[] = [];
 
     constructor(nameHouse: string, motto: string, numSoldiers: number){
         super(nameHouse, motto, numSoldiers);
