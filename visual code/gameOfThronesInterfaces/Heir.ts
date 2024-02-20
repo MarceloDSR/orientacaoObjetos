05b56596c43871b53a8fc1e801bdd22c5cba75a1
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-02-19 21:26:25 -0300

    Update Heir.ts

--- visual code/gameOfThronesInterfaces/Heir.ts
+++ visual code/gameOfThronesInterfaces/Heir.ts
@@ -4,5 +4,13 @@ export class Heir implements HouseHeir {
     heirName: string;
     heirAge: number;
     successionNumber: number;
+    heirs: HouseHeir[] = [];
+
+    constructor(heirName: string, heirAge: number, successionNumber: number){
+        this.heirName = heirName
+        this.heirAge = heirAge
+        this.successionNumber = successionNumber
+    }
+    
 
 }
