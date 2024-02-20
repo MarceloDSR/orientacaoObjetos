9fc1b05e04700fbe45173255fead60c221235d60
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-02-19 21:26:49 -0300

    Update Lannister.ts

--- visual code/gameOfThronesInterfaces/Lannister.ts
+++ visual code/gameOfThronesInterfaces/Lannister.ts
@@ -1,9 +1,12 @@
 import { NobleHouse } from "./NobleHouse";
+import { Heir } from "./Heir";
+import { HouseRules } from "./HouseRules";
 
-export class Lannister extends NobleHouse {
-    nameHouse: string; 
-    motto: string; 
-    numSoldiers: number;
+export class Lannister extends NobleHouse implements HouseRules {
+  nameHouse: string; 
+  motto: string; 
+  numSoldiers: number;
+  heirs: Heir[] = [];
 
     constructor(nameHouse: string, motto: string, numSoldiers: number){
         super(nameHouse, motto, numSoldiers);
