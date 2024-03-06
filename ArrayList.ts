a5255c7b74fd753f8d632916ac8e147cea147205
Marcelo Rodrigues <133075234+MarceloDSR@users.noreply.github.com>
2024-03-05 21:40:30 -0300

    Create ArrayList.ts

---
+++ ArrayList.ts
@@ -0,0 +1,14 @@
+import { IList } from "./Lista";
+
+export class ArrayList implements IList{
+    private items: any[]; // Array que armazena os itens da lista
+
+    constructor() {
+        this.items = [] // Inicializa a lista como um array vazio
+    }
+
+// Adiciona um elemento no final da lista
+    add(item: any): void{
+        
+    }; 
+}
